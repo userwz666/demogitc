08842ea5c2f1ca6f694831b9b25a23eb0e449046
GuanYi-Start <1119132301@qq.com>
2020-03-16 20:59:50 +0800

    张三提交02

--- src/main/java/com/example/demogitc/Test.java
+++ src/main/java/com/example/demogitc/Test.java
@@ -5,4 +5,8 @@ public class Test {
     public static void main(String[] args){
         System.out.println("1111111");
     }
+
+    public void method01(){
+        System.out.println("222222");
+    }
 }
