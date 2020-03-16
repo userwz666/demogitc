14364182d57870fb327350d4af1da16d49de29e8
GuanYi-Start <1119132301@qq.com>
2020-03-16 20:52:09 +0800

    张三提交01

---
+++ src/main/java/com/example/demogitc/DemogitcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogitc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogitcApplication.class, args);
+    }
+
+}
